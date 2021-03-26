0726190f9af5cce47f9c1064023df845b50d9cd4
hashimoto-junya <hashimoto-junya@dmm.com>
2021-03-26 16:53:45 +0900

    add: search form

---
+++ src/components/hits.tsx
@@ -0,0 +1,48 @@
+import { Link } from 'gatsby';
+import React from 'react';
+import styled from 'styled-components';
+import { connectHits } from 'react-instantsearch-dom';
+import { HitsProvided, Hit } from 'react-instantsearch-core';
+
+const StyleList = styled.ul`
+    background: #212121;
+    background-clip: content-box;
+    border-radius: 4px;
+    width: 100%;
+`;
+
+const StyleItem = styled.li`
+    list-style: none;
+    &:hover {
+        background-color: #303030;
+    }
+    height: 38px;
+    line-height: 38px;
+`;
+
+const StyledLink = styled(Link)`
+    display: block;
+    padding: 0 8px;
+    height: 100%;
+    color: #e6e6e6;
+    font-size: 13px;
+    text-decoration: none;
+`;
+
+const Hits: React.FC<HitsProvided<Hit>> = ({
+    hits,
+}) => {
+    return (
+        <StyleList>
+            {hits.map(hit => (
+                <StyleItem key={hit.id}>
+                    <StyledLink to={`/video/${hit.slug}`}>
+                        {hit.title}
+                    </StyledLink>
+                </StyleItem>
+            ))}    
+        </StyleList>
+    )
+};
+
+export default connectHits(Hits);
